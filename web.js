64c6fc3bd36f40117082a2a463dc669181118028
lathavairamani <lvairamani@paypal.com>
2013-07-09 22:32:31 +0530

    Updated web.js to read content from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  var data = fs.readFileSync('index.html');	
+  response.end(data);
 });
 
 var port = process.env.PORT || 5000;
